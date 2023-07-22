e6c5e2665fc166a256e1a3b72e065a3ce5bd9f47
{jaroga/} <139742802+WindowsSME@users.noreply.github.com>
2023-07-22 11:30:50 +0800

    Update XDR-Server-Status-Check.ps1

--- XDR-Server-Status-Check.ps1
+++ XDR-Server-Status-Check.ps1
@@ -1,6 +1,7 @@
 #sensor_xdr_check_server_status
 #Author : James Romeo Gaspar
 #OG 1.0 01Jul2023
+#Revision 2.0 20Jul2023 : Revised to handle null values
 $trapsPath = "C:\Program Files\Palo Alto Networks\Traps"
 if (-not (Test-Path $trapsPath)) {
     Write-Output "XDR not Installed : $(Get-Date)"
@@ -10,13 +11,23 @@ if (-not (Test-Path $trapsPath)) {
 $cytoolPath = Join-Path $trapsPath "cytool.exe"
 $commandOutput = & $cytoolPath websocket query
 $checkinOutput = & $cytoolPath checkin
-$serverResult = $commandOutput | Select-String -Pattern 'server: (.+)' | ForEach-Object { $_.Matches.Groups[1].Value }
-$serverResult = $serverResult -replace '^wss://|^https://|^http://'
-$serverResult = $serverResult -replace '/operations/socket$'
-$connected = $commandOutput | Select-String -Pattern "connected: (.+)" | ForEach-Object { $_.Matches.Groups[1].Value }
-$enabled = $commandOutput | Select-String -Pattern "enabled: (.+)" | ForEach-Object { $_.Matches.Groups[1].Value }
-if ($connected -eq "true" -and $enabled -eq "true") {
-    Write-Output "Server: $serverResult | Connection: True : $(Get-Date)"
-} else {
-    Write-Output "Server: $serverResult | Connection: Warning : $(Get-Date)"
+
+function Get-ValueFromCommandOutput {
+    param([string]$pattern)
+    $match = $commandOutput | Select-String -Pattern $pattern
+    if ($match) {
+        return $match.Matches.Groups[1].Value
+    }
+    return "Null|Error"
 }
+
+$serverResult = Get-ValueFromCommandOutput -pattern 'server: (.+)'
+if (![string]::IsNullOrEmpty($serverResult)) {
+    $serverResult = $serverResult -replace '^wss://|^https://|^http://'
+    $serverResult = $serverResult -replace '/operations/socket$'
+}
+
+$connected = Get-ValueFromCommandOutput -pattern "connected: (.+)"
+$enabled = Get-ValueFromCommandOutput -pattern "enabled: (.+)"
+
+Write-Output "Server: $serverResult | Connection: $connected | Enabled: $enabled : $(Get-Date)"
